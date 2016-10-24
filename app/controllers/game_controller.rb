fb8e33109e33a0b8ca690102ff8c6f2919da3335
JayaS <JayaS@users.noreply.github.com>
2016-10-24 16:41:47 -0500

    Commits

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,5 @@
 class GameController < ApplicationController
 def user_plays_rock
-
-
     moves =["rock", "paper", "scissors"]
     computer_move=moves.sample
   render("game/play_rock.html.erb")
@@ -9,6 +7,14 @@ end
 end
 
 def user_plays_paper
-
+  moves =["rock", "paper", "scissors"]
+  computer_move=moves.sample
+render("game/play_paper.html.erb")
+end
+end
+def user_plays_scissors
+  moves =["rock", "paper", "scissors"]
+  computer_move=moves.sample
+render("game/play_scissors.html.erb")
 end
 end
